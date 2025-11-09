1ddfd1860aa4942f0cdf271e889881eea79c7496
TD0961 <tensaedeme61@gmail.com>
2025-11-09 17:04:57 +0300

    updating picture2

--- frontend/src/sections/About.tsx
+++ frontend/src/sections/About.tsx
@@ -84,7 +84,7 @@ const About = () => {
           }`}
         >
           <img
-            src="/photo/sit-black.jpeg"
+            src="/photo/sit-black.jpg"
             alt="About Me"
             className="w-80 h-96 object-cover rounded-2xl shadow-2xl transform hover:rotate-3 hover:scale-105 transition-all duration-500"
           />
